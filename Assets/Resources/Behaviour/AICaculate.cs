d31e50c1a2a8c55b6d0e5b061a874183041191db
ALEXTANG <574809918@qq.com>
2022-01-26 14:50:30 +0800

    Update

---
+++ Assets/Resources/Behaviour/AICaculate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
+
+public class AICaculate : Action
+{
+    public SharedInt selfPower;
+
+    public override TaskStatus OnUpdate()
+    {
+        if (false)
+        {
+            return TaskStatus.Success;
+        }
+        else
+        {
+            return TaskStatus.Failure;
+        }
+    }
+}
